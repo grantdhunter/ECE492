89dcd467e39c195ebca41f9fe6169cf994281a25
Grant Hunter <gdhunter@ualberta.ca>
2014-02-11 12:41:37 -0700

    added comments to transmitterInterface.cpp

--- Group6_Project/Software/transmitterInterface.cpp
+++ Group6_Project/Software/transmitterInterface.cpp
@@ -15,72 +15,72 @@
  * TODO correct the Base address names and types
  */
 TransmitterInterface::TransmitterInterface(int* addrForward,int* addrBack,int* addrRight,int* addrLeft) {
-	// TODO Auto-generated constructor stub
-	forwardPin = addrForward;
-	backwardPin = addrBack;
-	leftPin = addrLeft;
-	rightPin = addrRight;
+ // TODO Auto-generated constructor stub
+ forwardPin = addrForward;
+ backwardPin = addrBack;
+ leftPin = addrLeft;
+ rightPin = addrRight;
 
-}
-/**
- *
+
+/*
+ * Destructor for the transmitterInterface object.
  */
 TransmitterInterface::~TransmitterInterface() {
-	// TODO Auto-generated destructor stub
+ // TODO Auto-generated destructor stub
 }
-
-/**
- *
- */
+/*
+ * Send turn right signal to transmitter. * Send turn left signal to transmitter.
+ */ 
 void TransmitterInterface::turnLeftOn() {
-	 IOWR_ALTERA_AVALON_PIO_DIRECTION(leftPin, ON);
+  IOWR_ALTERA_AVALON_PIO_DIRECTION(leftPin, ON);
 }
 
 /**
- *
+ * Send go forward signal to transmitter.
  */
 void TransmitterInterface::turnRightOn() {
-	IOWR_ALTERA_AVALON_PIO_DIRECTION(rightPin, ON);
+ IOWR_ALTERA_AVALON_PIO_DIRECTION(rightPin, ON);
 }
 
 /**
- *
+ * Send reverse signal to transmitter.
  */
 void TransmitterInterface::moveForwardOn() {
-	IOWR_ALTERA_AVALON_PIO_DIRECTION(forwardPin, ON);
+ IOWR_ALTERA_AVALON_PIO_DIRECTION(forwardPin, ON);
 }
 
 /**
- *
+ * Send stop turning left signal to transmitter.
  */
 void TransmitterInterface::moveBackwardOn() {
-	IOWR_ALTERA_AVALON_PIO_DIRECTION(backwardPin, ON);
+ IOWR_ALTERA_AVALON_PIO_DIRECTION(backwardPin, ON);
 }
 
 /**
- *
+ * Send stop turning right signal to transmitter.
  */
 void TransmitterInterface::turnLeftOff() {
-	 IOWR_ALTERA_AVALON_PIO_DIRECTION(leftPin, OFF);
+  IOWR_ALTERA_AVALON_PIO_DIRECTION(leftPin, OFF);
 }
 
-/**
- *
- */
+
+
+
 void TransmitterInterface::turnRightOff() {
-	IOWR_ALTERA_AVALON_PIO_DIRECTION(rightPin, OFF);
+ IOWR_ALTERA_AVALON_PIO_DIRECTION(rightPin, OFF);
 }
 
 /**
- *
+ * Send stop moving forwards signal to transmitter.
  */
 void TransmitterInterface::moveForwardOff() {
-	IOWR_ALTERA_AVALON_PIO_DIRECTION(forwardPin, OFF);
+ IOWR_ALTERA_AVALON_PIO_DIRECTION(forwardPin, OFF);
 }
 
 /**
- *
+ * Send stop moving backwards signal to transmitter.
  */
 void TransmitterInterface::moveBackwardOff() {
-	IOWR_ALTERA_AVALON_PIO_DIRECTION(backwardPin, OFF);
+ IOWR_ALTERA_AVALON_PIO_DIRECTION(backwardPin, OFF);
 }
+
