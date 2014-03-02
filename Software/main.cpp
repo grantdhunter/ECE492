bdeb2d360e3bd9ac1eb82c285b2c8a7458e786bc
Grant Hunter <info@grant-hunter.ca>
2014-03-02 16:43:48 -0700

    added license to main.cpp

--- Software/main.cpp
+++ Software/main.cpp
@@ -1,3 +1,13 @@
+/*
+ * main.cpp
+ *
+ * Very early draft
+ *
+ *  Created on: 2014-01-28
+ *      Author: gdhunter
+ */
+
+
 #include <stdio.h>
 #include "TransmitterInterface.h"
 #include "EmgInterface.h"
@@ -60,7 +70,6 @@ void taskEmg(void* pdata)
 void taskGyro(void* pdata)
 {
    
-    TransmitterInterface transmitter = nwe TransmitterInterface(TRANSMITTER_BASE_ADDRESS);
     long gyroAngle;
 
     imu.initialize();
@@ -141,3 +150,24 @@ int main(void)
     return 0;
 
 }
+//The MIT License (MIT)
+//
+//Copyright (c) 2014 Grant Hunter
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
