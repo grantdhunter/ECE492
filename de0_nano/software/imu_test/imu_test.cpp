3d82c8d8b53b6be1797484243ae4ef4fb0106a25
Grant Hunter <info@grant-hunter.ca>
2014-03-02 16:22:07 -0700

    worked on the imu test code

--- de0_nano/software/imu_test/imu_test.cpp
+++ de0_nano/software/imu_test/imu_test.cpp
@@ -27,12 +27,16 @@
  *     minutes per iteration.                                             *
  **************************************************************************/
 
+
+
+//Lots of help was taken from https://github.com/grantdhunter/i2cdevlib/blob/master/Arduino/MPU6050/Examples/MPU6050_DMP6/MPU6050_DMP6.ino
+
 #include <stdio.h>
 #include "includes.h"
 #include "system.h"
 
-#include "MPU6050.h"
-//#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "I2Cdev.h"
 
 /* Definition of Task Stacks */
 #define   TASK_STACKSIZE       2048
@@ -46,47 +50,75 @@ MPU6050 imu(IMU_BASE_ADDRESS);
 
 /* Prints "Hello World" and sleeps for three seconds */
 void task1(void* pdata) {
-
-	imu.initialize();
-	//True if every thing is good
-	if (imu.testConnection()) {
-
-		//TODO set offsets to zero the IMU
-		imu.setXGyroOffset();
-		imu.setYGyroOffset();
-		imu.setZGyroOffset();
-		imu.setZAccelOffset();
-	} else {
-		//Error
-		//TODO handle error
-	}
-
-	//Zero id everything is good
-	if (imu.dmpInitialize() == 0) {
-		imu.setDMPEnabled(true);
-
-	} else {
-		//Error
-		//TODO handle
-	}
-
-	while (1) {
-
-		unint8_t data = imu.getFIFOByte();
-
-		printf("IMU: %d\n", data);
-		OSTimeDlyHMSM(0, 0, 3, 0);
-	}
+    
+    uint8_t packetSize = 0;
+    unit16_t fifocount = 0;
+    unit8_t imuIntStatus;
+    unint8_t data[64];
+    Quaternion quat;
+    VectorFloat gravity;
+    float ypr[3];
+    
+    imu.initialize();
+    //True if every thing is good
+    if (imu.testConnection()) {
+        
+        //TODO set offsets to zero the IMU
+        imu.setXGyroOffset(X_OFFSET);
+        imu.setYGyroOffset(Y_OFFSET);
+        imu.setZGyroOffset(Z_OFFSET);
+        imu.setZAccelOffset(Z_OFFSET_A);
+    } else {
+        //Error
+        //TODO handle error
+    }
+    
+    //Zero id everything is good
+    if (imu.dmpInitialize() == 0) {
+        imu.setDMPEnabled(true);
+        
+        imuIntStatus = imu.getIntStatus();
+        
+        packetSize = imu.dmpGetFIFOPacketSize() 
+    } else {
+        //Error
+        //TODO handle
+    }
+    
+    
+    while (1) {
+        fifocount = mpu.getFIFOCount();
+        
+        if( imuIntStatus & 0x10 || fifocount == 1024){
+            //overflow
+            //TODO handle error
+        } else if(imuIntStatus & 0x02) {
+            while(fifocount < packetSize) {
+                fifocount = mpu.getFIFOCount();
+            }
+            
+             imu.getFIFOByte(data, packetSize);
+             fifocount -= packetSize;
+            
+            imu.dmpGetQuaternion(&quat, data);
+            imu.dmpGetGravity(&gravity, &quat);
+            imu.dmp.GetYawPitchRoll(ypr, &quat, &gravity);
+        }
+       
+        
+        printf("IMU: %d\n", data);
+        OSTimeDlyHMSM(0, 0, 3, 0);
+    }
 }
 
 /* The main function creates two task and starts multi-tasking */
 int main(void) {
-
-	OSTaskCreateExt(task1, NULL, &task1_stk[TASK_STACKSIZE - 1], TASK1_PRIORITY,
-			TASK1_PRIORITY, task1_stk, TASK_STACKSIZE, NULL, 0);
-
-	OSStart();
-	return 0;
+    
+    OSTaskCreateExt(task1, NULL, &task1_stk[TASK_STACKSIZE - 1], TASK1_PRIORITY,
+                    TASK1_PRIORITY, task1_stk, TASK_STACKSIZE, NULL, 0);
+    
+    OSStart();
+    return 0;
 }
 
 /******************************************************************************
